d15340536cc8d1d2bdcaafa118ebf0baa5afe3a3
abdelrahman167565 <64866520+abdelrahman167565@users.noreply.github.com>
2021-01-13 20:45:57 +0200

    reservation not completed 13-1

---
+++ src/touringsystem/Reservation.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package touringsystem;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+
+/**
+ *
+ * @author pudu1
+ */
+public class Reservation {
+
+    private int ID;
+    private String reservationDate;
+//  private Traveler traveler;
+//  private Hotel hotel;
+//  private Airline airline;
+//  private Transporation transportation;
+//  private Packages package;
+//    private Payment payment;
+
+//  public Reservation(int id, String date,Hotel hotel,Airline airline,Transportation transport,Traveler trav)
+//  {
+//      
+//  }
+    public Reservation(int idو, Traveler trav, String date, Hotel hotel, Airline airline) {
+//database connection
+        MongoClient client = new MongoClient();
+        MongoDatabase TouringSystem = client.getDatabase("TouringSystem");
+        MongoCollection reservartion = TouringSystem.getCollection("Reservation");
+        Document reservation = new Document("ID", id).
+    }
+    }
+
+    public String CancelReservation(int id) {
+        MongoClient client = new MongoClient();
+        MongoDatabase TouringSystem = client.getDatabase("TouringSystem");
+        MongoCollection reservartion = TouringSystem.getCollection("Reservation");
+    reservartion.deleteOne(Filters.eq("ID",id));
+}
